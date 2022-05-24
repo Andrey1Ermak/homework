8695536d112e74466fd5254e0421c691a705b9d6
Andrew Ermak <ead200@yandex.ru>
2022-05-24 19:24:04 +0300

    fix 3.5

--- 3.5.py
+++ 3.5.py
@@ -6,12 +6,12 @@ def my_funct():
         my_list = list(numbers.split())
         if my_list.count('q') > 0:
             ex = 1
-            print(f"Сумма введенных чисел: {result}")
             break
         else:
             my_list = list(map(int, my_list))
             result = result + sum(my_list)
-            print(f"Сумма введенных чисел: {result}")
+        print(f"Сумма введенных чисел на данный момент: {result}")
+    return result
 
 
-my_funct()
+print(f"Финальная сумма - {my_funct()}")
